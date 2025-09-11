23ff971e8d7d6f89e6b47cc95ce3c60fdbaedd56
tall15421542-lab <tall15421542@gmail.com>
2025-09-11 10:45:58 +0800

    use fastapi to serve model

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import requests
 import torch
 import os
 from PIL import Image
 from torch.nn.functional import cosine_similarity
 from transformers import CLIPModel, CLIPProcessor
-
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 # Load pre-trained CLIP model and processor
 model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
@@ -21,32 +21,42 @@ images = [
     Image.open(image_url) for image_url in image_urls
 ]
 
+
+app = FastAPI()
+
+class SearchRequest(BaseModel):
+    text: str
+
+class SearchResponse(BaseModel):
+    image: str
+
+@app.post("/search", response_model=SearchResponse)
+def search_similar_image(searchRequest: SearchRequest):
 # Prepare search term
-text = "cat"
+    text = searchRequest.text 
 
 # Preprocess inputs
-inputs = processor(text=text, images=images, return_tensors="pt", padding=True)
+    inputs = processor(text=text, images=images, return_tensors="pt", padding=True)
 
 # Generate embeddings
-with torch.no_grad():
-    outputs = model(**inputs)
+    with torch.no_grad():
+        outputs = model(**inputs)
 
 # Extract and normalize image and text embeddings
-image_embeddings = outputs.image_embeds / outputs.image_embeds.norm(
-    dim=-1, keepdim=True
-)
-text_embeddings = outputs.text_embeds / outputs.text_embeds.norm(dim=-1, keepdim=True)
+    image_embeddings = outputs.image_embeds / outputs.image_embeds.norm(
+        dim=-1, keepdim=True
+    )
+    text_embeddings = outputs.text_embeds / outputs.text_embeds.norm(dim=-1, keepdim=True)
 
 # Compute cosine similarity
-cosine_scores = cosine_similarity(image_embeddings, text_embeddings)
+    cosine_scores = cosine_similarity(image_embeddings, text_embeddings)
 
 # Output similarity scores
-for i, image_url in enumerate(image_urls):
-    print(
-        f"Cosine similarity between the '{text}' and '{image_url}': {cosine_scores[i]}"
-    )
+    for i, image_url in enumerate(image_urls):
+        print(
+            f"Cosine similarity between the '{text}' and '{image_url}': {cosine_scores[i]}"
+        )
 
 # Output the most similar image
-most_similar_index = torch.argmax(cosine_scores).item()
-print(f"The most similar image is: {image_urls[most_similar_index]}")
-
+    most_similar_index = torch.argmax(cosine_scores).item()
+    return { "image": image_urls[most_similar_index] }
